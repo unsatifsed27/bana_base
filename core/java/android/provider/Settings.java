115b4718bdd0d934d1d33920143e141cefc535e5
LorDClockaN <lordclockan@gmail.com>
2017-09-19 19:27:58 +0800

    Powermenu Torch option [1/2]
    Change-Id: Ib65e075bbbc1e3f2ff8d98ab0770f827b9c397bc

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5597,6 +5597,12 @@ public final class Settings {
         public static final String POWERMENU_USERS = "powermenu_users";
 
         /**
+         * Whether to display the torch option in the power menu
+         * @hide
+         */
+        public static final String POWERMENU_TORCH = "powermenu_torch";
+
+        /**
          * Whether to display the users option in the power menu
          * @hide
          */
