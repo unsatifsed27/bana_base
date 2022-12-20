GalaxyNight/android_frameworks_base

QS: Add Sound tile
    abc ezio84: adapt to O, show volume panel with long press,
    use full dnd (no interruptions) as silent mode

    @beanstown106: Sound Tile improvements
    *added longpress action
    *added more descriptive labels for each state instead of always saying sound

    ezio84: Adapt for N
    Change-Id: I46f4f8cc62683144a9bd714ca67e2a0f46940d96

Squashed commit of the following:

commit 69f86d9a38b5a942e3f9c914895ee45c0079e8e9
Author: maxwen <max.weninger@gmail.com>
Date:   Fri Apr 13 03:32:51 2018 +0200

    base: SystemUI: stop crashing qs tiles during boot

    this triggers my ocd
    continues a579c2430f1a0bf088023b4283dcec37776c070d

    Change-Id: I31719b595cb50a330f26e25074243e04c06f63c1
    Signed-off-by: xyyx <xyyx@mail.ru>

commit f419619c033fa99a809f53aa82dbdd7d6732177c
Author: xyyx <xyyx@mail.ru>
Date:   Thu Sep 28 11:55:36 2017 +0800

    SoundTile: Change ZEN_MODE_NO_INTERRUPTIONS to ZEN_MODE_ALARMS

    Change-Id: I5edaaa4d551630049b29e09d3cecfb5ffd503e99

commit 916645271f57c47d60bb0a04c4e51776b51a18f1
Author: Yoshinori Hirano <yoshinori.hirano@sonymobile.com>
Date:   Sun Sep 18 22:33:08 2016 +0200

    Add Sound tile to Quick Settings

    abc ezio84: adapt to O, show volume panel with long press,
    use full dnd (no interruptions) as silent mode

    @beanstown106: Sound Tile improvements
    *added longpress action
    *added more descriptive labels for each state instead of always saying sound

    ezio84: Adapt for N
    Change-Id: I46f4f8cc62683144a9bd714ca67e2a0f46940d96

Change-Id: I178bfa69ff181f0f65f94ffae1444aaea5f21a80
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

packages/SystemUI/src/com/android/systemui/qs/tiles/SoundTile.java
@@ -1,0 +1,182 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.systemui.qs.tiles;
+
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.media.AudioManager;
+import android.os.Handler;
+import android.os.Looper;
+import android.provider.Settings.Global;
+import android.service.quicksettings.Tile;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+import com.android.internal.logging.MetricsLogger;
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+
+import com.android.systemui.Dependency;
+import com.android.systemui.qs.QSHost;
+import com.android.systemui.plugins.qs.QSTile.BooleanState;
+import com.android.systemui.qs.tileimpl.QSTileImpl;
+import com.android.systemui.R;
+import com.android.systemui.statusbar.policy.ZenModeController;
+import com.android.systemui.dagger.qualifiers.Background;
+import com.android.systemui.dagger.qualifiers.Main;
+import com.android.systemui.plugins.ActivityStarter;
+import com.android.systemui.plugins.FalsingManager;
+import com.android.systemui.plugins.statusbar.StatusBarStateController;
+import com.android.systemui.qs.logging.QSLogger;
+
+import javax.inject.Inject;
+
+public class SoundTile extends QSTileImpl<BooleanState> {
+
+    private final ZenModeController mZenController;
+    private final AudioManager mAudioManager;
+
+    private boolean mListening = false;
+
+    private BroadcastReceiver mReceiver;
+    private IntentFilter mFilter;
+
+    @Inject
+    public SoundTile(
+            QSHost host,
+            @Background Looper backgroundLooper,
+            @Main Handler mainHandler,
+            FalsingManager falsingManager,
+            MetricsLogger metricsLogger,
+            StatusBarStateController statusBarStateController,
+            ActivityStarter activityStarter,
+            QSLogger qsLogger
+    ) {
+        super(host, backgroundLooper, mainHandler, falsingManager, metricsLogger,
+                statusBarStateController, activityStarter, qsLogger);
+        mZenController = Dependency.get(ZenModeController.class);
+        mAudioManager = (AudioManager) mContext.getSystemService(Context.AUDIO_SERVICE);
+        mReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                refreshState();
+            }
+        };
+    }
+
+    @Override
+    public BooleanState newTileState() {
+        return new BooleanState();
+    }
+
+    @Override
+    public void handleSetListening(boolean listening) {
+        if (mAudioManager == null) {
+            return;
+        }
+        if (mListening == listening) return;
+        mListening = listening;
+        if (listening) {
+            final IntentFilter filter = new IntentFilter();
+            filter.addAction(AudioManager.INTERNAL_RINGER_MODE_CHANGED_ACTION);
+            mContext.registerReceiver(mReceiver, filter);
+        } else {
+            mContext.unregisterReceiver(mReceiver);
+        }
+    }
+
+    @Override
+    protected void handleClick(@Nullable View view) {
+        updateState();
+    }
+
+    @Override
+    public void handleLongClick(@Nullable View view) {
+        mAudioManager.adjustVolume(AudioManager.ADJUST_SAME, AudioManager.FLAG_SHOW_UI);
+    }
+
+    @Override
+    public Intent getLongClickIntent() {
+        return null;
+    }
+
+    private void updateState() {
+        int oldState = mAudioManager.getRingerModeInternal();
+        int newState = oldState;
+        switch (oldState) {
+            case AudioManager.RINGER_MODE_NORMAL:
+                newState = AudioManager.RINGER_MODE_VIBRATE;
+                mAudioManager.setRingerModeInternal(newState);
+                break;
+            case AudioManager.RINGER_MODE_VIBRATE:
+                newState = AudioManager.RINGER_MODE_SILENT;
+                mZenController.setZen(Global.ZEN_MODE_ALARMS, null, TAG);
+                break;
+            case AudioManager.RINGER_MODE_SILENT:
+                newState = AudioManager.RINGER_MODE_NORMAL;
+                mZenController.setZen(Global.ZEN_MODE_OFF, null, TAG);
+                mAudioManager.setRingerModeInternal(newState);
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public CharSequence getTileLabel() {
+        return mContext.getString(R.string.quick_settings_sound_label);
+    }
+
+    @Override
+    protected void handleUpdateState(BooleanState state, Object arg) {
+        if (mAudioManager == null) {
+            return;
+        }
+        switch (mAudioManager.getRingerModeInternal()) {
+            case AudioManager.RINGER_MODE_NORMAL:
+                state.icon = ResourceIcon.get(R.drawable.ic_qs_ringer_audible);
+                state.label = mContext.getString(R.string.quick_settings_sound_ring);
+                state.contentDescription =  mContext.getString(
+                        R.string.quick_settings_sound_ring);
+                state.state = Tile.STATE_ACTIVE;
+                break;
+            case AudioManager.RINGER_MODE_VIBRATE:
+                state.icon = ResourceIcon.get(R.drawable.ic_qs_ringer_vibrate);
+                state.label = mContext.getString(R.string.quick_settings_sound_vibrate);
+                state.contentDescription =  mContext.getString(
+                        R.string.quick_settings_sound_vibrate);
+                state.state = Tile.STATE_INACTIVE;
+                break;
+            case AudioManager.RINGER_MODE_SILENT:
+                state.icon = ResourceIcon.get(R.drawable.ic_qs_ringer_silent);
+                state.label = mContext.getString(R.string.quick_settings_sound_dnd);
+                state.contentDescription =  mContext.getString(
+                        R.string.quick_settings_sound_dnd);
+                state.state = Tile.STATE_INACTIVE;
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.CRDROID_SETTINGS;
+    }
+}

unsatifsed27/bana_base

Signed-off-by: Rizky Benggolo <travarilo@gmail.com>

packages/SystemUI/src/com/android/systemui/qs/tiles/SoundTile.java
@@ -1,0 +1,182 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.systemui.qs.tiles;
+
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.media.AudioManager;
+import android.os.Handler;
+import android.os.Looper;
+import android.provider.Settings.Global;
+import android.service.quicksettings.Tile;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+import com.android.internal.logging.MetricsLogger;
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+
+import com.android.systemui.Dependency;
+import com.android.systemui.qs.QSHost;
+import com.android.systemui.plugins.qs.QSTile.BooleanState;
+import com.android.systemui.qs.tileimpl.QSTileImpl;
+import com.android.systemui.R;
+import com.android.systemui.statusbar.policy.ZenModeController;
+import com.android.systemui.dagger.qualifiers.Background;
+import com.android.systemui.dagger.qualifiers.Main;
+import com.android.systemui.plugins.ActivityStarter;
+import com.android.systemui.plugins.FalsingManager;
+import com.android.systemui.plugins.statusbar.StatusBarStateController;
+import com.android.systemui.qs.logging.QSLogger;
+
+import javax.inject.Inject;
+
+public class SoundTile extends QSTileImpl<BooleanState> {
+
+    private final ZenModeController mZenController;
+    private final AudioManager mAudioManager;
+
+    private boolean mListening = false;
+
+    private BroadcastReceiver mReceiver;
+    private IntentFilter mFilter;
+
+    @Inject
+    public SoundTile(
+            QSHost host,
+            @Background Looper backgroundLooper,
+            @Main Handler mainHandler,
+            FalsingManager falsingManager,
+            MetricsLogger metricsLogger,
+            StatusBarStateController statusBarStateController,
+            ActivityStarter activityStarter,
+            QSLogger qsLogger
+    ) {
+        super(host, backgroundLooper, mainHandler, falsingManager, metricsLogger,
+                statusBarStateController, activityStarter, qsLogger);
+        mZenController = Dependency.get(ZenModeController.class);
+        mAudioManager = (AudioManager) mContext.getSystemService(Context.AUDIO_SERVICE);
+        mReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                refreshState();
+            }
+        };
+    }
+
+    @Override
+    public BooleanState newTileState() {
+        return new BooleanState();
+    }
+
+    @Override
+    public void handleSetListening(boolean listening) {
+        if (mAudioManager == null) {
+            return;
+        }
+        if (mListening == listening) return;
+        mListening = listening;
+        if (listening) {
+            final IntentFilter filter = new IntentFilter();
+            filter.addAction(AudioManager.INTERNAL_RINGER_MODE_CHANGED_ACTION);
+            mContext.registerReceiver(mReceiver, filter);
+        } else {
+            mContext.unregisterReceiver(mReceiver);
+        }
+    }
+
+    @Override
+    protected void handleClick(@Nullable View view) {
+        updateState();
+    }
+
+    @Override
+    public void handleLongClick(@Nullable View view) {
+        mAudioManager.adjustVolume(AudioManager.ADJUST_SAME, AudioManager.FLAG_SHOW_UI);
+    }
+
+    @Override
+    public Intent getLongClickIntent() {
+        return null;
+    }
+
+    private void updateState() {
+        int oldState = mAudioManager.getRingerModeInternal();
+        int newState = oldState;
+        switch (oldState) {
+            case AudioManager.RINGER_MODE_NORMAL:
+                newState = AudioManager.RINGER_MODE_VIBRATE;
+                mAudioManager.setRingerModeInternal(newState);
+                break;
+            case AudioManager.RINGER_MODE_VIBRATE:
+                newState = AudioManager.RINGER_MODE_SILENT;
+                mZenController.setZen(Global.ZEN_MODE_ALARMS, null, TAG);
+                break;
+            case AudioManager.RINGER_MODE_SILENT:
+                newState = AudioManager.RINGER_MODE_NORMAL;
+                mZenController.setZen(Global.ZEN_MODE_OFF, null, TAG);
+                mAudioManager.setRingerModeInternal(newState);
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public CharSequence getTileLabel() {
+        return mContext.getString(R.string.quick_settings_sound_label);
+    }
+
+    @Override
+    protected void handleUpdateState(BooleanState state, Object arg) {
+        if (mAudioManager == null) {
+            return;
+        }
+        switch (mAudioManager.getRingerModeInternal()) {
+            case AudioManager.RINGER_MODE_NORMAL:
+                state.icon = ResourceIcon.get(R.drawable.ic_qs_ringer_audible);
+                state.label = mContext.getString(R.string.quick_settings_sound_ring);
+                state.contentDescription =  mContext.getString(
+                        R.string.quick_settings_sound_ring);
+                state.state = Tile.STATE_ACTIVE;
+                break;
+            case AudioManager.RINGER_MODE_VIBRATE:
+                state.icon = ResourceIcon.get(R.drawable.ic_qs_ringer_vibrate);
+                state.label = mContext.getString(R.string.quick_settings_sound_vibrate);
+                state.contentDescription =  mContext.getString(
+                        R.string.quick_settings_sound_vibrate);
+                state.state = Tile.STATE_INACTIVE;
+                break;
+            case AudioManager.RINGER_MODE_SILENT:
+                state.icon = ResourceIcon.get(R.drawable.ic_qs_ringer_silent);
+                state.label = mContext.getString(R.string.quick_settings_sound_dnd);
+                state.contentDescription =  mContext.getString(
+                        R.string.quick_settings_sound_dnd);
+                state.state = Tile.STATE_INACTIVE;
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.BANANADROID;
+    }
+}